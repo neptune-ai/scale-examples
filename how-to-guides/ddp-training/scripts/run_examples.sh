19cb6e43cad427968c7c1c971b28490e46206c10
LeoRoccoBreedt <leo.breedt@neptune.ai>
2025-04-10 10:21:00 +0200

    chore: add requirements and bash scripts and update GH worflow config for testing scripts

---
+++ how-to-guides/ddp-training/scripts/run_examples.sh
@@ -0,0 +1,7 @@
+set -e
+
+echo "Installing requirements..."
+pip install -U -r requirements.txt
+
+echo "Running train_ddp_single_run.py..."
+torchrun --nproc_per_node=2 --nnodes=1 train_ddp_single_run.py
