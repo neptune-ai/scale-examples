e8184d8be5a939f87680d5c51b428d50235a42a1
LeoRoccoBreedt <leo.breedt@neptune.ai>
2025-04-16 11:21:39 +0200

    chore: remove reference to scale

--- how-to-guides/quickstart/scripts/run_examples.sh
+++ how-to-guides/quickstart/scripts/run_examples.sh
@@ -4,4 +4,4 @@ echo "Installing requirements..."
 pip install -Uq -r requirements.txt
 
 echo "Running neptune_scale_quickstart.py..."
-python neptune_scale_quickstart.py
+python neptune_quickstart.py
