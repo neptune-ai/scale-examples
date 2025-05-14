3665d41ad3ce42ac4bc1eded65fe68cc07cd7f57
Siddhant Sadangi <siddhant.sadangi@neptune.ai>
2025-05-14 16:27:02 +0200

    Update how-to-guides/quickstart/scripts/run_examples.sh
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Signed-off-by: Siddhant Sadangi <siddhant.sadangi@gmail.com>

--- how-to-guides/quickstart/scripts/run_examples.sh
+++ how-to-guides/quickstart/scripts/run_examples.sh
@@ -4,5 +4,5 @@ echo "Installing requirements..."
 pip install -Uq -r requirements.txt
 
 echo "Running neptune_scale_quickstart.py..."
-curl -o sample.png https://neptune.ai/wp-content/uploads/2024/05/blog_feature_image_046799_8_3_7_3-4.jpg
+curl -L -o sample.png https://neptune.ai/wp-content/uploads/2024/05/blog_feature_image_046799_8_3_7_3-4.jpg
 python neptune_quickstart.py
